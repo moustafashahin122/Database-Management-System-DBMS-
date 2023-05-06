a19acdf8eb560737948b621410d6bcbf00cc2d67
moustafashahin122 <65954226+moustafashahin122@users.noreply.github.com>
2023-05-06 08:20:31 +0300

    made the createDB function

--- database.sh
+++ database.sh
@@ -1,63 +1,81 @@
-#!/bin/bash 
+#!/bin/bash
+mixedRegex="^[a-zA-Z][a-zA-Z0-9]+$"
+numRegex="^[0-9]+$"
+stringRegex="^[a-zA-Z]+$"
+function mainMenu {
+  clear
 
+  echo "DBMS MAIN MENU"
 
-echo "DBMS MAIN MENU";
+  echo "select the operation ***********"
+  echo "  1) Create database"
+  echo "  2) List Databases"
+  echo "  3) connect to database"
+  echo "  4) Drop database"
 
-echo "select the operation ***********"
-echo "  1) Create database"
-echo "  2) List Databases"
-echo "  3) connect to database"
-echo "  4)Drop database" 
+  read n
 
-read n
-  if [ $n -eq 1 ]
-  then
- echo "you choose to create a new database" 
-  read -p "enter the Database name " dbName 
-  mkdir $dbName 
-   elif [ $n -eq 2 ]
-  then
-   echo "you choose to list database"
-  ls 
-  
-   elif [ $n -eq 3 ]
-  then
-  echo "You choose to connect to database"
-  read -p "enter a data base name to connect " dbConnect
-   cd $dbConnect 
-   elif [ $n -eq 4 ]
-  then
-   echo "you choose to drop database"
-  read -p "Enter a databse name to delete " dbDelete
-  if [ -d $dbDelete ]
-   then rmdir  $dbDelete 
-   else 
-   echo "Database is not exist"
-   fi
-  else 
-   echo "invalid option"
+  if [ $n -eq 1 ]; then
+    createDB
+
+  elif [ $n -eq 2 ]; then
+    echo "you choose to list database"
+    ls -d */
+  # for files ls p |grep -v /
+  elif [ $n -eq 3 ]; then
+    echo "You choose to connect to database"
+    read -p "enter a data base name to connect " dbConnect
+    cd $dbConnect
+  elif [ $n -eq 4 ]; then
+    echo "you choose to drop database"
+    read -p "Enter a databse name to delete " dbDelete
+    if [ -d $dbDelete ]; then
+      rmdir $dbDelete
+    else
+      echo "Database is not exist"
+    fi
+  else
+    echo "invalid option"
   fi
+}
 
+function createDB {
+  read -rp "enter the Database name " dbName
 
+  while ! $(validInput "${dbName}" "${mixedRegex}"); do
+    echo please enter a valid input
+    read -rp "enter the Database name " dbName
+  done
+  if [ ! -d "${dbName}" ]; then
+    mkdir "${dbName}"
 
+    echo " you've created ${dbName}"
+  else
+    echo "data base already exists"
+  fi
+}
 
-##read n
-##case $n in
-  ##1) echo "you choose to create a new database" 
-  ##read -p "enter the Database name " dbName 
-  ##mkdir $dbName 
-  ##;;
-  ##2) echo "you choose to list database"
-  ##ls 
-  ##;;
-  ##3) echo "You choose to connect to database"
-  ##read dbConnect
-  ##cd $dbConnect 
-  ##;;
-  ##4) echo "you choose to drop database"
-  ##read -p "Enter a databse name to delete " dbDelete 
-  ##rmdir  $dbDelete
-  ##;;
-  ##*) echo "invalid option"
-  ##;;
-##esac
+# function listDBs{
+
+# }
+# function dropDB{
+
+# }
+# function connectDB{
+
+# }
+# general check to make sure that the user input is valid
+# -doesn't start with a number
+# -doesn't containt special characters
+# no spaces
+
+function validInput {
+  # this function taske two argument the first is a string you want to check and the second is a regex
+  if [[ $1 =~ ${2} ]]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+mainMenu
---
+++ try.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# function validInput {
+#   regex="^[a-zA-Z][a-zA-Z0-9]+$"
+
+#   if [[ $1 =~ ${regex} ]]; then
+#     echo 1
+#   else
+#     echo 0
+#   fi
+# }
+
+# vvv=$(validInput mmm)
+# if [[ $vvv == 1 ]];
+# then
+
+# echo $vvv
+# fi
+
+if ((1)); then
+    echo 0
+fi
