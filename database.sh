b2e0984028b6cdde4290a240f10155773dad621a
moustafashahin122 <65954226+moustafashahin122@users.noreply.github.com>
2023-05-08 16:27:04 +0300

    main menu

--- database.sh
+++ database.sh
@@ -7,56 +7,45 @@ currentDB=""
 echo ${scriptDir}
 
 function mainMenu {
-
   echo "DBMS MAIN MENU"
-
   echo "select the operation ***********"
   echo "  1) Create database"
   echo "  2) List Databases"
   echo "  3) connect to database"
   echo "  4) Drop database"
-
-  read n
-
+  read -r n
   if [ $n -eq 1 ]; then
     createDB
-
   elif [ $n -eq 2 ]; then
     echo "you choose to list database"
     listDBs
   # for files ls p |grep -v /
-
   elif [ $n -eq 3 ]; then
     connectDB
-
   elif [ $n -eq 4 ]; then
     dropDB
-
   fi
 }
 
 function createDB {
-  read -rp "enter the Database name " dbName
-
+  echo "enter the Database name: "
+  read -r dbName
   while ! $(validInput "${dbName}" "${mixedRegex}"); do
-    echo please enter a valid input
-    read -rp "enter the Database name " dbName
+    echo "please enter a valid input"
+    read -r dbName
   done
   if [ ! -d "${dbName}" ]; then
     mkdir -p "${scriptDir}"/databases/"${dbName}"
-
     echo " you've created ${dbName}"
-
   else
     echo "data base already exists"
-
   fi
 }
 function connectDB {
-
   echo "You choose to connect to database"
   listDBs
-  read -rp "enter a data base name to connect " dbName
+  echo "enter a data base name to connect "
+  read -r dbName
   if [ -d "${scriptDir}"/databases/"${dbName}" ]; then
     cd databases/${dbName}
     echo " you're connected to ${dbName}"
@@ -70,12 +59,12 @@ function listDBs {
   cd "${scriptDir}"/databases
   ls
   cd "${scriptDir}"
-
 }
 function dropDB {
   echo "you choose to drop database"
   listDBs
-  read -rp "Enter a databse name to delete " dbDelete
+  echo "Enter a databse name to delete "
+  read -r dbDelete
   if [ -d "${scriptDir}"/databases/"${dbDelete}" ]; then
     rm -r "${scriptDir}"/databases/"${dbDelete}"
     echo "${dbDelete}" deleted successfully
