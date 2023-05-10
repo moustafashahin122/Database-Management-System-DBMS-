f2377f483782b57612ea726a67f8fd66c3033a77
moustafashahin122 <65954226+moustafashahin122@users.noreply.github.com>
2023-05-10 09:10:41 +0300

    last main

--- test/table.sh
+++ test/table.sh
@@ -1,58 +1,45 @@
 #!/bin/bash
 scriptDir=${PWD}
 
-function createTable{ 
+function createTable {
 
-echo "Enter a table name"
-read -r tableName
-mkdir -p ${scriptDir}/Tables/${tableName}
+  echo "Enter a table name"
+  read -r tableName
+  touch "${tableName}.db"
 }
 
-
-
-
-
-
-
-echo "tables MAIN MENU";
+echo "tables MAIN MENU"
 
 echo "select the operation for table"
 echo "  1) Create Table "
 echo "  2) List Tables"
 echo "  3) Drop Table"
-echo "  4)Insert into Table" 
-echo "  5)Select From Table" 
-echo "  6)Delete From Table" 
-echo "  7)Update Table" 
+echo "  4) Insert into Table"
+echo "  5) Select From Table"
+echo "  6) Delete From Table"
+echo "  7) Update Table"
 
 read n
-  if [ $n -eq 1 ]
-  then
- echo "you choose to Create Table" 
-  createTable()
+if [ $n -eq 1 ]; then
+  echo "you choose to Create Table"
+  createTable
 
-   elif [ $n -eq 2 ]
-  then
-   echo "you choose to list tables"
-  ls 
-  
-   elif [ $n -eq 3 ]
-  then
+elif [ $n -eq 2 ]; then
+  echo "you choose to list tables"
+  ls
+
+elif [ $n -eq 3 ]; then
   echo "You choose to Drop Table"
-   elif [ $n -eq 4 ]
-  then
-   echo "you choose to Insert into Table"
-   elif [ $n -eq 5 ]
-  then
-   echo "you choose to Select From Table"
-   elif [ $n -eq 6 ]
-  then
-   echo "you choose to Delete From Table "
-   elif [ $n -eq 7 ]
-  then
-   echo "you choose to Update Table"
- 
-  else 
-   echo "invalid option"
-  fi
- 
+
+elif [ $n -eq 4 ]; then
+  echo "you choose to Insert into Table"
+elif [ $n -eq 5 ]; then
+  echo "you choose to Select From Table"
+elif [ $n -eq 6 ]; then
+  echo "you choose to Delete From Table "
+elif [ $n -eq 7 ]; then
+  echo "you choose to Update Table"
+
+else
+  echo "invalid option"
+fi
